7394ef3648cdb4e609492883a4725250fb93c513
Ali Sabet <alsab.ass1995@gmail.com>
2025-02-26 17:04:58 +0330

    ready for vercel

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,6 @@
 import Image from "next/image";
 
 export default function Home() {
-  try {
-    prisma.$connect();
-    console.log("Database connected successfully.");
-  } catch (error) {
-    console.error("Failed to connect to the database:", error);
-  }
   return (
     <div>
       <h1>Hello</h1>
